73708c95124b421b96d204f9f0ea757f33c8a7c0
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-04-27 22:37:18 -0300

    Correção

--- desafio013.py
+++ desafio013.py
@@ -5,4 +5,4 @@ salario = float(input("Quanto é o seu salário: "))
 aumento = salario * 0.15
 novo_salario = salario + aumento
 
-print(f"Seu salário com aumento de 15% ficou R$ {novo_salario:.2f   }")
+print(f"Seu salário com aumento de 15% ficou R$ {novo_salario:.2f}")
