7d10e981c6f59901e5fafc6fb8a36dabd5aeef19
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-05-10 17:49:37 -0300

    Aula 10

---
+++ aula10.py
@@ -0,0 +1,6 @@
+nome = str(input("Qual é o seu nome? "))
+if nome == "Gustavo":
+    print("Que nome lindo você tem!")
+else:
+    print("Que nome comum você tem!")
+print("Bom dia, {}!".format(nome))
