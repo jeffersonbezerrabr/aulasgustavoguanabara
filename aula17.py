7d2021873cf30c8f3191e061e07b64ab9aa14226
Jefferson Bezerra <jeffersonbezerra88@hotmail.com>
2025-07-17 23:38:19 -0300

    Aula 17

---
+++ aula17.py
@@ -0,0 +1,29 @@
+# Vídeo https://www.youtube.com/watch?v=N1hTsbW50eM&list=PLHz_AreHm4dksnH2jVTIVNviIMBVYyFnH&index=8&t=624s
+
+# num = [2, 5, 9, 1]
+# num[2] = 3
+# num.append(7)
+# num.sort(reverse=True)
+# num.insert(2, 2)
+# removido = num.pop()
+# if 4 in num:
+#     num.remove(4)
+# else:
+#     print("Não achei o número 4")
+# print(num)
+# print(f"Essa lista tem {len(num)} elementos.")
+# print(f"O número removido pelo pop foi {removido}")
+
+# valores = []
+# for cont in range(0, 5):
+#     valores.append(input("Digite um valor: "))
+
+# for i, v in enumerate(valores):
+#     print(f"Na posição {i} encontrei o valor {v}")
+# print("Cheguei ao final da lista.")
+
+a = [2, 3, 4, 7]
+b = a[:]
+b[2] = 8
+print(f"Lista A: {a}")
+print(f"Lista B: {b}")
