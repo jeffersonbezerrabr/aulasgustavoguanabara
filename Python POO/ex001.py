965e9039c5125509fc942a16bca000472dc812cd
jeffersonbezerrabr <jeffersonbezerra88@hotmail.com>
2026-01-18 17:07:14 -0300

    Exercício 01

---
+++ Python POO/ex001.py
@@ -0,0 +1,30 @@
+# Declaração de Classe
+class Gafanhoto:
+    def __init__(self): # Método Construtor
+        # Atributos de Instância
+        self.nome = ""
+        self.idade = 0
+
+    # Métodos de Instância
+    def aniversario(self):
+        self.idade += 1
+        
+    def mensagem(self):
+        return f"{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade."
+
+# Declaração de Objetos
+g1 = Gafanhoto()
+g1.nome = "Maria"
+g1.idade = 17
+g1.aniversario()
+print(g1.mensagem())
+
+g2 = Gafanhoto()
+g2.nome = "Mauro"
+g2.idade = 53
+print(g2.mensagem())
+
+g3 = Gafanhoto()
+g3.nome = "Jeff"
+g3.idade = 37
+print(g3.mensagem())
